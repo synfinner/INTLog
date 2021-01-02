9b4b788f12d8ad2d13a364463bb99282edaea304
synfinner <synfinner@synfinner.ru>
2021-01-02 14:54:17 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# INTLog
+
+INTLog is a simple Flask app designed to keep track of potentially interesting artifacts during an investigation. 
+
+This application was designed to help me keep track of artifacts that I may stumble across during an investigation. I was originally using things like notepad, maltego, jupyter, and a bunch of other apps. The problem I ran into is that I needed a simple wheel, not a nuclear reactor to log what I was seeing. Secondly, I wanted something that could be hosted locally and data kept on my disk. And just like that, INTLog was born. Simplicity is the name of the game here. This app caters specifically to my needs and maybe it will help you too.
+
+## Setup
+
+Install requirements: `pip3 install -r requirements.txt`
+
+### Setup SQLite
+
+Create a new SQLite db within the `data` directory named `intlog.sqlite`
+
+**Setup the investigations table:**
+
+```
+CREATE TABLE investigations (
+  id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+  investigation_name varchar(128),
+  investigation_date timestamp(128),
+  investigation_desc text(250)
+)
+```
+
+**Setup the artifacts table:**
+
+```
+CREATE TABLE "artifacts" (
+	"id"	integer NOT NULL,
+	"investigation_id"	integer(128),
+	"artifact_name"	char(128),
+	"artifact_type"	char(128),
+	"artifact_desc"	varchar(250),
+	"artifact_reference"	varchar(250),
+	"artifact_date"	timestamp(128),
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("investigation_id") REFERENCES "investigations"("id")
+)
+```
---
+++ intlog.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import url_for
+from flask import flash
+from flask import redirect
+from flask import make_response
+from flask_fontawesome import FontAwesome
+import sqlite3
+from werkzeug.exceptions import abort
+import datetime
+import io
+import csv
+
+def get_db_connection():
+    conn = sqlite3.connect('data/intlog.sqlite')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'WTFAMIDOINGWITHMYLIFE'
+app.debug = True
+fa = FontAwesome(app)
+
+def get_inv(inv_id):
+    conn = get_db_connection()
+    investigation = conn.execute('SELECT * FROM investigations WHERE id = ?',
+                        (inv_id,)).fetchone()
+    conn.close()
+    if investigation is None:
+        abort(404)
+    return investigation
+
+def get_artifacts(inv_id):
+    conn = get_db_connection()
+    artifacts = conn.execute('SELECT * FROM artifacts WHERE investigation_id = ? ORDER BY artifact_date DESC',
+                        (inv_id,)).fetchall()
+    conn.close()
+    if artifacts is None:
+        abort(404)
+    return artifacts
+
+def get_artifact(article_id):
+    conn = get_db_connection()
+    artifacts = conn.execute('SELECT * FROM artifacts WHERE id = ?',
+                        (article_id,)).fetchone()
+    conn.close()
+    if artifacts is None:
+        abort(404)
+    return artifacts
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        inv_name = request.form['investigation_name']
+        inv_desc = request.form['investigation_description']
+        date = datetime.datetime.now().replace(microsecond=0).isoformat()
+        if not inv_name:
+            flash('Investigation name is required!!!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO investigations (investigation_name, investigation_date, investigation_desc) VALUES (?, ?, ?)',
+                         (inv_name, date, inv_desc))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+    return render_template('create.html')
+
+@app.route('/<int:inv_id>/add_artifact',methods=('GET', 'POST'))
+def add_artifact(inv_id):
+    if request.method == 'POST':
+        #artifact = request.form['artifact']
+        artifact_type = request.form['type']
+        artifact_data = request.form['artifact_data']
+        artifact_desc = request.form['artifact_description']
+        date = datetime.datetime.now().replace(microsecond=0).isoformat()
+        if not artifact_data:
+            flash('An artifact is required!!!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO artifacts (artifact_type, artifact_reference, artifact_desc, investigation_id, artifact_date) VALUES (?, ?, ?, ?, ?)',
+                         (artifact_type, artifact_data, artifact_desc,inv_id,date))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('investigation',inv_id = inv_id))
+    return render_template('add_artifact.html')
+
+@app.route('/<int:inv_id>/export_artifacts',methods=('GET', 'POST'))
+def export_artifacts(inv_id):
+    conn = get_db_connection()
+    si = io.StringIO()
+    cw = csv.writer(si)
+    rows = conn.execute('SELECT artifact_reference, artifact_type, artifact_desc, artifact_date FROM artifacts WHERE investigation_id = ?', (inv_id,)).fetchall()
+    cw.writerows(rows)
+    output = make_response(si.getvalue())
+    output.headers["Content-Disposition"] = "attachment; filename=artifact_export.csv"
+    output.headers["Content-type"] = "text/csv"
+    conn.close()
+    return output
+
+@app.route('/delete_artifact/<int:artifact_id>',methods=('POST','GET'))
+def delete_artifact(artifact_id):
+    conn = get_db_connection()
+    conn.execute('DELETE FROM artifacts WHERE id = ?', (artifact_id,))
+    conn.commit()
+    conn.close()
+    return redirect(request.referrer)
+
+@app.route('/delete_investigation/<int:inv_id>',methods=('POST','GET'))
+def delete_investigation(inv_id):
+    conn = get_db_connection()
+    conn.execute('DELETE FROM investigations WHERE id = ?', (inv_id,))
+    conn.commit()
+    try:
+        conn.execute('DELETE FROM artifacts WHERE investigation_id = ?', (inv_id,))
+        conn.commit()
+    except:
+        pass
+    conn.close()
+    flash('Deleted investigation!')
+    return redirect(url_for('index'))
+
+@app.route('/edit_artifact/<int:artifact_id>',methods=('POST','GET'))
+#@app.route('/investigation/<int:inv_id>/edit_artifact/<int:artifact_id>',methods=('POST','GET'))
+def edit_artifact(artifact_id):
+    args = request.args
+    artifact = get_artifact(artifact_id)
+    if request.method == 'POST':
+        #artifact = request.form['artifact']
+        artifact_type = request.form['type']
+        artifact_data = request.form['artifact_data']
+        artifact_desc = request.form['artifact_description']
+        #date = datetime.datetime.now().replace(microsecond=0).isoformat()
+        if not artifact_data:
+            flash('An artifact is required!!!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE artifacts SET artifact_type=?, artifact_reference=?, artifact_desc=? WHERE id = ?',
+                         (artifact_type, artifact_data, artifact_desc,artifact_id))
+            conn.commit()
+            conn.close()
+            flash('Removed artifact!')
+            if len(args) > 0:
+                return redirect(url_for('investigation',inv_id = args['inv_id']))
+            else:
+                return redirect(url_for('index'))
+    return render_template('edit_artifact.html',artifact=artifact)
+
+
+@app.route('/<int:inv_id>')
+def investigation(inv_id):
+    investigation = get_inv(inv_id)
+    artifacts = get_artifacts(inv_id)
+    return render_template('investigation.html', investigation=investigation,artifacts=artifacts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/')
+#@app.route('/page/<int:page>')
+def index(page=1):
+    #return "wtf"
+    conn = get_db_connection()
+    investigations = conn.execute('SELECT * FROM investigations ORDER BY investigation_date DESC').fetchall()
+    #print(investigations)
+    conn.close()
+    return render_template('index.html',investigations=investigations)
+
+
+
